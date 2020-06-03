b2a447a2179b3663bc28028982081d1d42c52b84
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-02 22:41:26 -0300

    Items index endpoint

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -2,10 +2,10 @@ import Knex from 'knex'
 
 export const seed = async (knex: Knex) => {
   await knex('items').insert([
-    { title: 'Lâmpadas', image: 'lampadas.svg' },
-    { title: 'Pilhas e baterias', image: 'baterias.svg' },
-    { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
-    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
+    { title: 'Lâmpadas', image: 'lamp-bulbs.svg' },
+    { title: 'Pilhas e baterias', image: 'batteries.svg' },
+    { title: 'Papéis e Papelão', image: 'paper.svg' },
+    { title: 'Resíduos Orgânicos', image: 'organics.svg' },
     { title: 'Óleo de Cozinha', image: 'oleo.svg' }
   ])
 }
--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,15 @@ import express from 'express'
 
 const routes = express.Router()
 
-routes.get('/', (request, response) => {
-  response.json(['Marina', 'Ferreira', 'hohoho', 'oioioi', 'eieiei'])
+routes.get('/items', async (request, response) => {
+  const db = request.app.get('db')
+  const items = await db('items').select('*')
+  const serializedItems = items.map(item => ({
+    title: item.title,
+    image_url: `http://localhost:3333/uploads/${item.image}`
+  }))
+
+  return response.json(serializedItems)
 })
 
 export default routes
