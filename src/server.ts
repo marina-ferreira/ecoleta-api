85e8285eee9a9bde2da55eceab2c68dff961f0e8
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-02 22:01:18 -0300

    Serve images

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path'
+
 import routes from './routes'
 import connection from './database/connection'
 
@@ -6,6 +8,8 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.set("db", connection)
 
