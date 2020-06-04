39f85c023e4a46f0f171c340628e59bcc6e01387
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-04 06:34:58 -0300

    Installs Cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import path from 'path'
+import cors from 'cors'
 
 import routes from './routes'
 import connection from './database/connection'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
