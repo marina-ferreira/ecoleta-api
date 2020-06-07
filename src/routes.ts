1b6190ff53f20a067103f03614dfe9152bad8fb7
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-07 09:32:12 -0300

    Point create validations

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import multer from 'multer'
-import multerConfig from './config/router'
+import { celebrate, Joi } from 'celebrate'
 
+import multerConfig from './config/router'
 import PointsController from './controllers/points_controller'
 import ItemsController from './controllers/items_controller'
 
@@ -14,7 +15,25 @@ const itemsController = new ItemsController()
 routes.get('/items', itemsController.index)
 
 routes.get('/points', pointsController.index)
-routes.post('/points', upload.single('image'), pointsController.create)
 routes.get('/points/:id', pointsController.show)
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required(),
+      items: Joi.string().required()
+    })
+  }, {
+    abortEarly: false,
+  }),
+  pointsController.create
+)
 
 export default routes
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
+import { errors } from 'celebrate'
 
 import routes from './routes'
 import connection from './database/connection'
@@ -14,5 +15,7 @@ app.use(routes)
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.set("db", connection)
+
+app.use(errors())
 
 app.listen(3333)
